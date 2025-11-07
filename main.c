b90dba50e7a6db969318b194268994c08aaf4ba0
Totoro <33295757+Mooninghnk@users.noreply.github.com>
2025-11-06 23:47:33 -0500

    0.0.3
    added more functionality

--- main.c
+++ main.c
@@ -3,49 +3,82 @@
 
 // Structure to hold all our widgets
 typedef struct {
+    // Wallet tab
     GtkWidget *entry_address;
     GtkWidget *label_balance;
     GtkWidget *label_balance_usd;
+
+    // Security tab
+    GtkWidget *entry_password;
+    GtkWidget *entry_privkey;
+    GtkWidget *entry_wif;
+
     BitcoinWallet *current_wallet;
+    gboolean password_set;
+    GtkWidget *balance;
+    GtkWidget *tx_count;
+    GtkWidget *check_balance_button;
+
 } AppWidgets;
 
 // Callback for copying text to clipboard
 static void copy_to_clipboard(GtkWidget *button, gpointer user_data) {
     GtkEntry *entry = GTK_ENTRY(user_data);
     const gchar *text = gtk_entry_get_text(entry);
-    
+
     GtkClipboard *clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
     gtk_clipboard_set_text(clipboard, text, -1);
-    
-    g_print("Copied to clipboard: %s\n", text);
+
+    g_print("Copied to clipboard\n");
+}
+
+// Callback for setting password
+static void set_password(GtkWidget *button, gpointer user_data) {
+    AppWidgets *widgets = (AppWidgets *)user_data;
+    const gchar *password = gtk_entry_get_text(GTK_ENTRY(widgets->entry_password));
+
+    if (strlen(password) < 6) {
+        g_print("Password too short! Use at least 6 characters.\n");
+        return;
+    }
+
+    widgets->password_set = TRUE;
+    g_print("Password set successfully!\n");
+
+    // Clear password field for security
+    gtk_entry_set_text(GTK_ENTRY(widgets->entry_password), "");
 }
 
 // Callback for generating a new wallet
 static void generate_wallet(GtkWidget *button, gpointer user_data) {
     AppWidgets *widgets = (AppWidgets *)user_data;
-    
+
     // Free previous wallet if exists
     if (widgets->current_wallet) {
         bitcoin_wallet_free(widgets->current_wallet);
         widgets->current_wallet = NULL;
     }
-    
+
     // Generate new wallet
     widgets->current_wallet = bitcoin_wallet_create();
-    
+
     if (!widgets->current_wallet) {
         g_printerr("Failed to create wallet!\n");
         return;
     }
-    
-    // Update address field
-    gtk_entry_set_text(GTK_ENTRY(widgets->entry_address), 
+
+    // Update Wallet tab
+    gtk_entry_set_text(GTK_ENTRY(widgets->entry_address),
                        widgets->current_wallet->mainnet_address);
-    
-    // Balance starts at 0
     gtk_label_set_text(GTK_LABEL(widgets->label_balance), "0.00000000 BTC");
     gtk_label_set_text(GTK_LABEL(widgets->label_balance_usd), "≈ $0.00 USD");
-    
+
+    // Update Security tab
+    gtk_entry_set_text(GTK_ENTRY(widgets->entry_privkey),
+                       widgets->current_wallet->privkey_hex);
+    gtk_entry_set_text(GTK_ENTRY(widgets->entry_wif),
+                       widgets->current_wallet->wif);
+
     g_print("New wallet generated!\n");
     g_print("Address: %s\n", widgets->current_wallet->mainnet_address);
 }
@@ -53,19 +86,19 @@ static void generate_wallet(GtkWidget *button, gpointer user_data) {
 // Cleanup function called when window is destroyed
 static void cleanup(GtkWidget *widget, gpointer user_data) {
     AppWidgets *widgets = (AppWidgets *)user_data;
-    
+
     // Free wallet if exists
     if (widgets->current_wallet) {
         bitcoin_wallet_free(widgets->current_wallet);
         widgets->current_wallet = NULL;
     }
-    
+
     // Cleanup Bitcoin library
     bitcoin_cleanup();
-    
+
     // Free the widgets structure
     g_free(widgets);
-    
+
     gtk_main_quit();
 }
 
@@ -74,16 +107,16 @@ int main(int argc, char *argv[]) {
     GObject *window;
     GObject *button;
     GError *error = NULL;
-    
+
     // Initialize Bitcoin library
     if (!bitcoin_init()) {
         g_printerr("Failed to initialize Bitcoin library!\n");
         return 1;
     }
-    
+
     // Initialize GTK
     gtk_init(&argc, &argv);
-    
+
     // Create builder and load UI file
     builder = gtk_builder_new();
     if (gtk_builder_add_from_file(builder, "builder.ui", &error) == 0) {
@@ -92,39 +125,54 @@ int main(int argc, char *argv[]) {
         bitcoin_cleanup();
         return 1;
     }
-    
+
     // Allocate structure to hold widgets
     AppWidgets *widgets = g_new0(AppWidgets, 1);
     widgets->current_wallet = NULL;
-    
+    widgets->password_set = FALSE;
+
     // Get window
     window = gtk_builder_get_object(builder, "window");
-    
-    // Get widgets
+
+    // Get Wallet tab widgets
     widgets->entry_address = GTK_WIDGET(gtk_builder_get_object(builder, "entry_address"));
     widgets->label_balance = GTK_WIDGET(gtk_builder_get_object(builder, "label_balance"));
     widgets->label_balance_usd = GTK_WIDGET(gtk_builder_get_object(builder, "label_balance_usd"));
-    
-    // Connect generate button
+
+    // Get Security tab widgets
+    widgets->entry_password = GTK_WIDGET(gtk_builder_get_object(builder, "entry_password"));
+    widgets->entry_privkey = GTK_WIDGET(gtk_builder_get_object(builder, "entry_privkey"));
+    widgets->entry_wif = GTK_WIDGET(gtk_builder_get_object(builder, "entry_wif"));
+
+    // Connect Wallet tab buttons
     button = gtk_builder_get_object(builder, "btn_generate");
     g_signal_connect(button, "clicked", G_CALLBACK(generate_wallet), widgets);
-    
-    // Connect copy button
+
     button = gtk_builder_get_object(builder, "btn_copy_address");
     g_signal_connect(button, "clicked", G_CALLBACK(copy_to_clipboard), widgets->entry_address);
-    
+
+    // Connect Security tab buttons
+    button = gtk_builder_get_object(builder, "btn_set_password");
+    g_signal_connect(button, "clicked", G_CALLBACK(set_password), widgets);
+
+    button = gtk_builder_get_object(builder, "btn_copy_privkey");
+    g_signal_connect(button, "clicked", G_CALLBACK(copy_to_clipboard), widgets->entry_privkey);
+
+    button = gtk_builder_get_object(builder, "btn_copy_wif");
+    g_signal_connect(button, "clicked", G_CALLBACK(copy_to_clipboard), widgets->entry_wif);
+
     // Connect quit button
     button = gtk_builder_get_object(builder, "btn_quit");
     g_signal_connect(button, "clicked", G_CALLBACK(cleanup), widgets);
-    
+
     // Connect window destroy signal
     g_signal_connect(window, "destroy", G_CALLBACK(cleanup), widgets);
-    
+
     // We don't need the builder anymore
     g_object_unref(builder);
-    
+
     // Start GTK main loop
     gtk_main();
-    
+
     return 0;
 }
